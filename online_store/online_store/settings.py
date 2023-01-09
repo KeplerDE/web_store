b49bdcfb8ae32ff847371e8d779bced6647b8af5
keplerde <keplerde@gmail.com>
2023-01-09 12:30:17 +0100

    include templates + static

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -115,13 +115,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-
-
+# Method 1
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "../static")
 ]
-
+# Method 2
+STATICFILES_DIRS = [
+    BASE_DIR / "static/"
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
--- online_store/online_store/urls.py
+++ online_store/online_store/urls.py
@@ -3,13 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('app.urls', namespace='myapp'))
-
+    path('', include('app.urls', namespace='myapp')),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
